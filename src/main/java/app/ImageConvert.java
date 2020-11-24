04568b0af4149f91373832fc4d596c1b536b7433
secondwatch <watch_ch@163.com>
2020-11-25 00:04:00 +0800

    fix audio

--- src/main/java/app/ImageConvert.java
+++ src/main/java/app/ImageConvert.java
@@ -64,22 +64,24 @@ public class ImageConvert extends BaseConvert {
     public void Image2Image(String inImagePath, String outImagePath) {
         try {
             File file = new File(inImagePath);
-            BufferedImage bufferedImage = ImageIO.read(file);
-            StringBuffer sb = new StringBuffer();
-            int width = bufferedImage.getWidth();
-            int height = bufferedImage.getHeight();
-            int minx = bufferedImage.getMinX();
-            int miny = bufferedImage.getMinY();
-            for (int j = miny; j < height; j += SPEED) {
-                for (int k = minx; k < width; k += SPEED) {
-                    int pixel = bufferedImage.getRGB(k, j);
-                    char ascii = RGB2Gray.conver(pixel);
-                    sb.append(ascii);
+            if(file.exists()){
+                BufferedImage bufferedImage = ImageIO.read(file);
+                StringBuffer sb = new StringBuffer();
+                int width = bufferedImage.getWidth();
+                int height = bufferedImage.getHeight();
+                int minx = bufferedImage.getMinX();
+                int miny = bufferedImage.getMinY();
+                for (int j = miny; j < height; j += SPEED) {
+                    for (int k = minx; k < width; k += SPEED) {
+                        int pixel = bufferedImage.getRGB(k, j);
+                        char ascii = RGB2Gray.conver(pixel);
+                        sb.append(ascii);
+                    }
+                    sb.append("\r\n");
                 }
-                sb.append("\r\n");
+                String SUFFIX = SPEED + "-";
+                txt2img(width, height, sb.toString(), outImagePath, SPEED, true);
             }
-            String SUFFIX = SPEED + "-";
-            txt2img(width, height, sb.toString(), outImagePath, SPEED, true);
         } catch (IOException e) {
             e.printStackTrace();
             //TODO:替换为log4j
